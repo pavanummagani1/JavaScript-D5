9bcec51e3d2a62327192632ded8593d4551278e0
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-11-23 13:17:50 +0530

    STRING METHODS MCQs

---
+++ script.js
@@ -0,0 +1,121 @@
+//MCQ-1
+console.log("Hello World".replace("World", "Universe"));
+
+//MCQ-2
+console.log("foo bar baz".replaceAll("bar", "baz"));
+
+//MCQ-3
+console.log("Hello".charAt(4));
+
+//MCQ-4
+console.log("Hello World".charAt(-1));
+
+//MCQ-5
+console.log("Hello".at(-1));
+
+//MCQ-6
+console.log("Hello World".length);
+
+//MCQ-7
+console.log("Javascript".slice(4,10));
+
+//MCQ-8
+console.log("Javascript".slice(-6));
+
+//MCQ-9
+console.log("Javascript".substring(4,10));
+
+//MCQ-10
+console.log("Hello".substring(-2));
+
+//MCQ-11
+console.log('Hello'.charCodeAt(1));
+
+//MCQ-12
+console.log("Hello".toUpperCase());
+
+//MCQ-13
+console.log("Hello World".split(" "));
+
+//MCQ-14
+console.log("a,b,c".split(","));
+
+//MCQ-15
+console.log("abcabcabc".split("a",2));
+
+//MCQ-16
+console.log("Javascript is Fun".indexOf("is"));
+
+//MCQ-17
+console.log("Javascript".slice(-4,-1));
+
+//MCQ-18
+console.log("123456789".substr(3,4));
+
+//MCQ-19
+console.log("JavaScript".startsWith("Java"));
+
+//MCQ-20
+console.log("JavaScript".endsWith("Script"));
+
+//MCQ-21
+console.log("   HELLO   ".trim());
+
+//MCQ-22
+console.log("abcDEF".toLowerCase());
+
+//MCQ-23
+console.log("Hello".concat(" ","World"));
+
+//MCQ-24
+console.log("Hello"[0]);
+
+//MCQ-25
+console.log("Hello".charAt(10));
+
+//MCQ-26
+console.log("Hello World".includes("World"));
+
+//MCQ-27
+console.log("Hello".repeat(3));
+
+//MCQ-28
+console.log("abc123".search(/[0-9]/));
+
+//MCQ-29
+console.log("   ".trim().length);
+
+//MCQ-30
+console.log("JavaScript".replace("Script", "Fun"));
+
+//MCQ-31
+console.log("Hello".padStart(10,"*"));
+
+//MCQ-32
+console.log("Hello".padEnd(10,"!"));
+
+//MCQ-33
+console.log("JAvaScript is Cool".lastIndexOf("is"));
+
+//MCQ-34
+console.log("Test123".startsWith("T",1));
+
+//MCQ-35
+console.log("Test123".endsWith("3",6));
+
+//MCQ-36
+console.log("Javascript".slice(4,-2));
+
+//MCQ-37
+console.log("abcdef".split("",3));
+
+//MCQ-38
+console.log("   SpacedOut   ".trimStart());
+
+//MCQ-39
+console.log("repeat".repeat(0));
+
+//MCQ-40
+console.log("12345".indexOf(6));
+
+
